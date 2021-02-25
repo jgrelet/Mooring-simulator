112315cd56454819eedd59ff01e862088ed30d77
Jacques Grelet <jacques.grelet@gmail.cpm>
2021-02-25 10:06:05 +0100

    add mooring example

---
+++ examples/luckyscale_2020_2.py
@@ -0,0 +1,76 @@
+8 BMTI-100 0
+11 Shackle 1/2 0
+10 Chain 18mm 1.5
+11 Shackle 1/2 0
+12 Steel 6,5 mm 20.0
+11 Shackle 1/2 0
+19 Benthos 5+chaine 4m 0
+11 Shackle 1/2 0
+12 Steel 6,5 mm 120.0 clamp 11 Capteur_RBR0.0;
+12 Steel 6,5 mm 200.0 clamp 8 Microcat0.0;
+12 Steel 6,5 mm 160.0 clamp 11 Capteur_RBR0.0;
+11 Shackle 1/2 0
+19 Benthos 5+chaine 4m 0
+11 Shackle 1/2 0
+12 Steel 6,5 mm 95.0 clamp 11 Capteur_RBR0.0;
+12 Steel 6,5 mm 50.0 clamp 11 Capteur_RBR0.0;
+12 Steel 6,5 mm 50.0 clamp 11 Capteur_RBR0.0;
+12 Steel 6,5 mm 25.0 clamp 11 Capteur_RBR0.0;
+12 Steel 6,5 mm 25.0 clamp 11 Capteur_RBR0.0;
+11 Shackle 1/2 0
+9 Aquamicro 0
+11 Shackle 1/2 0
+12 Steel 6,5 mm 25.0 clamp 11 Capteur_RBR0.0;
+12 Steel 6,5 mm 25.0 clamp 11 Capteur_RBR0.0;
+12 Steel 6,5 mm 24.0 clamp 11 Capteur_RBR0.0;
+11 Shackle 5/8 0
+9 1 Release 0
+13 pear link 5/8 0
+11 Shackle 5/8 0
+10 Nylon 18mm 6.0
+11 Shackle 5/8 0
+10 Chain 18mm 2.0
+12 1 Rain train 0
+depth_instru
+0.0
+0
+0
+0
+0
+0
+0
+0
+0_-900.0
+0_-1000.0
+0_-1200.0
+0
+0
+0
+0_-1400.0
+0_-1450.0
+0_-1500.0
+0_-1550.0
+0_-1575.0
+0
+0.0
+0
+0_-1625.0
+0_-1650.0
+0_-1670.0
+0
+0
+0
+0
+0
+0
+0
+0
+fin
+
+3
+0.0;0.0:
+-500.0;0.0:
+-1685.0;0.0:
+290;
+1;
+-1685;
