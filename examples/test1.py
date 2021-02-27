257a136e735769a313922cb1ca90cabeb9182a88
Jacques Grelet <jacques.grelet@gmail.cpm>
2021-02-27 20:04:52 +0100

    add test example

---
+++ examples/test1.py
@@ -0,0 +1,21 @@
+10 WH75- FLA2 0
+12 Steel 8,5 mm 680.0
+10 Chain 13mm 10.0
+9 Benthos_3 0
+10 Chain 18mm 10.0
+12 1 Rain train 0
+depth_instru
+-300.0
+0
+0
+0
+0
+0
+fin
+
+2
+0.0;1.0:
+-1000.0;0.0:
+1000;
+1;
+-1000.0;
